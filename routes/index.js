15f669edf482741f8025427ead24eaf2b94eba85
deathgaze <ksefchik.dev@gmail.com>
2014-05-22 01:54:36 -0500

    fix empty query bug

--- routes/index.js
+++ routes/index.js
@@ -70,151 +70,181 @@ exports.registerRoutes = function(app, Parse) {
 
 	app.get('/profile', function (req,res) {
 		var username = req.cookies.get("username");
+		if (username) {
+			// Get private user info
+			var q = new Parse.Query(Parse.User);
+			q.equalTo("username", username);
+			q.first({
+				success: function (result) {
+					var userInfo = {
+						email: result.get("email"),
+						major: result.get("major"),
+						emailVerified: result.get("emailVerified")
+					};
+					verifyLoginCookieAndSendPage(req, res, "profile", "User Profile", userInfo);
+				},
+				error: function(error) {
+					// No user info to send, so redirect to login page 
+					// (they shouldn't be here anyways)
+					res.location("/login");
+					res.redirect("/login");
+				}
+			});				
+		} else {
+			// No login cookie set. Punt to login.
+			res.location("/login");
+			res.redirect("/login");			
+		}
 
-		// Get private user info
-		var q = new Parse.Query(Parse.User);
-		q.equalTo("username", username);
-		q.first({
-			success: function (result) {
-				var userInfo = {
-					email: result.get("email"),
-					major: result.get("major"),
-					emailVerified: result.get("emailVerified")
-				};
-				verifyLoginCookieAndSendPage(req, res, "profile", "User Profile", userInfo);
-			},
-			error: function(error) {
-				// No user info to send, so redirect to login page 
-				// (they shouldn't be here anyways)
-				res.location("/login");
-				res.redirect("/login");
-			}
-		});	
 	});
 
 	app.post("/profile/update/email", function (req, res) {
-		var q = new Parse.Query(Parse.User).equalTo("username", req.cookies.get("username"));
-		q.first({
-			success: function(user) {
-				if (isValidCookie(req, user)) {
-					Parse.User.become(req.cookies.get("sessionToken")).then(function (user) {
-						user.setEmail(req.body.email);
-						user.save(null, {
-							success: function (user) {
-								console.log("User " + user.get("username") + " updated email to " 
-									+ req.body.email);
-								res.location("/profile");
-								res.redirect("/profile");
-							}, 
-							error: function (user, error) {
-								console.log("Failed saving user " + user.get("username")
-									+ " email address, error " + error.code + ": " + 
-									error.message);
-								res.location("/profile");
-								res.redirect("/profile");
-							}
+		var username = req.cookies.get("username");
+		if (username) {
+			var q = new Parse.Query(Parse.User).equalTo("username", username);
+			q.first({
+				success: function(user) {
+					if (isValidCookie(req, user)) {
+						Parse.User.become(req.cookies.get("sessionToken")).then(function (user) {
+							user.setEmail(req.body.email);
+							user.save(null, {
+								success: function (user) {
+									console.log("User " + user.get("username") + " updated email to " 
+										+ req.body.email);
+									res.location("/profile");
+									res.redirect("/profile");
+								}, 
+								error: function (user, error) {
+									console.log("Failed saving user " + user.get("username")
+										+ " email address, error " + error.code + ": " + 
+										error.message);
+									res.location("/profile");
+									res.redirect("/profile");
+								}
+							});
+						}, function (error) {
+							// no cookie?
+							res.location("/login");
+							res.redirect("/login");
 						});
-					}, function (error) {
-						// no cookie?
-						res.location("/login");
-						res.redirect("/login");
-					});
-					
-				}
-				else
-				{
-					// Not a valid token. Punt it.
+						
+					}
+					else
+					{
+						// Not a valid token. Punt it.
+						res.location("/logout");
+						res.redirect("/logout");
+					}
+				},
+				error: function(error) {
+					// Not a valid user. Punt it.
 					res.location("/logout");
 					res.redirect("/logout");
 				}
-			},
-			error: function(error) {
-				// Not a valid user. Punt it.
-				res.location("/logout");
-				res.redirect("/logout");
-			}
-		});
+			});			
+		} else {
+			// Cookie not set (somehow). Punt to login screen.
+			res.location("/login");
+			res.redirect("/login");
+		}
+
 	});
 
 	app.post("/profile/update/major", function (req, res) {
-		var q = new Parse.Query(Parse.User).equalTo("username", req.cookies.get("username"));
-		q.first({
-			success: function(user) {
-				if (isValidCookie(req, user)) {
-					Parse.User.become(req.cookies.get("sessionToken")).then(function (user) {
-						user.set("major", req.body.major);
-						user.save(null, {
-							success: function(user) {
-								console.log("User " + user.get("username") + " updated major to "
-								 	+ req.body.major);
-								res.location("/profile");
-								res.redirect("/profile");		
-							},
-							error: function(user, error) {
-								console.log("User " + user.get("username") + " error updating major: " 
-									+ error.code + ": " + error.message);
-								res.location("/profile");
-								res.redirect("/profile");
-							}
-						});	
-					}, function (error) {
-						console.log("Error becoming user " + user.get("username") 
-							+ ": " + error.code + " - " + error.message);
-					});
-				}
-				else
-				{
-					// Not a valid token. Punt it.
+		var username = req.cookies.get("username");
+		if (username) {
+			var q = new Parse.Query(Parse.User).equalTo("username", username);
+			q.first({
+				success: function(user) {
+					if (isValidCookie(req, user)) {
+						Parse.User.become(req.cookies.get("sessionToken")).then(function (user) {
+							user.set("major", req.body.major);
+							user.save(null, {
+								success: function(user) {
+									console.log("User " + user.get("username") + " updated major to "
+									 	+ req.body.major);
+									res.location("/profile");
+									res.redirect("/profile");		
+								},
+								error: function(user, error) {
+									console.log("User " + user.get("username") + " error updating major: " 
+										+ error.code + ": " + error.message);
+									res.location("/profile");
+									res.redirect("/profile");
+								}
+							});	
+						}, function (error) {
+							console.log("Error becoming user " + user.get("username") 
+								+ ": " + error.code + " - " + error.message);
+						});
+					}
+					else
+					{
+						// Not a valid token. Punt it.
+						res.location("/logout");
+						res.redirect("/logout");
+					}
+				},
+				error: function(error) {
+					// Not a valid user. Punt it.
 					res.location("/logout");
 					res.redirect("/logout");
 				}
-			},
-			error: function(error) {
-				// Not a valid user. Punt it.
-				res.location("/logout");
-				res.redirect("/logout");
-			}
-		});
+			});			
+		} else {
+			// Cookie not set (somehow). Punt to login screen.
+			res.location("/login");
+			res.redirect("/login");
+		}
+
 	});
 
 	app.post("/profile/update/password", function (req, res) {
-		var q = new Parse.Query(Parse.User).equalTo("username", req.cookies.get("username"));
-		q.first({
-			success: function(user) {
-				if (isValidCookie(req, user)) {
-					Parse.User.become(req.cookies.get("sessionToken")).then(function (user) {
-						user.setPassword(req.body.password);
-						user.save(null, {
-							success: function(user) {
-								console.log("User " + user.get("username") + " updated password");
-								res.location("/profile");
-								res.redirect("/profile");
-							},
-							error: function(user, error) {
-								console.log("Password Save Error " + error.code + ": " 
-									+ error.message);
-								res.location("/profile");
-								res.redirect("/profile");
-							}
-						});						
-					}, function (error) {
-						console.log("Failed to authenticate user session token, Error " 
-							+ error.code + ": " + error.message);
-					});
-				}
-				else
-				{
-					// Not a valid token. Punt it.
+		var username = req.cookies.get("username");
+		if (username) {
+			var q = new Parse.Query(Parse.User).equalTo("username", username);
+			q.first({
+				success: function(user) {
+					if (isValidCookie(req, user)) {
+						Parse.User.become(req.cookies.get("sessionToken")).then(function (user) {
+							user.setPassword(req.body.password);
+							user.save(null, {
+								success: function(user) {
+									console.log("User " + user.get("username") + " updated password");
+									res.location("/profile");
+									res.redirect("/profile");
+								},
+								error: function(user, error) {
+									console.log("Password Save Error " + error.code + ": " 
+										+ error.message);
+									res.location("/profile");
+									res.redirect("/profile");
+								}
+							});						
+						}, function (error) {
+							console.log("Failed to authenticate user session token, Error " 
+								+ error.code + ": " + error.message);
+						});
+					}
+					else
+					{
+						// Not a valid token. Punt it.
+						res.location("/logout");
+						res.redirect("/logout");
+					}
+				},
+				error: function(error) {
+					// Not a valid user. Punt it.
 					res.location("/logout");
 					res.redirect("/logout");
 				}
-			},
-			error: function(error) {
-				// Not a valid user. Punt it.
-				res.location("/logout");
-				res.redirect("/logout");
-			}
-		});
+			});
+		} else {
+			// Cookie not set (somehow). Punt to login screen.
+			res.location("/login");
+			res.redirect("/login");
+		}
+
 	});	
 
 	app.get("/logout", function (req,res){
@@ -253,25 +283,33 @@ exports.registerRoutes = function(app, Parse) {
 	// saved for later -- might need it
 	app.get("/login.api", function (req, res) {
 		// Get private user info
-		var q = new Parse.Query(Parse.User).equalTo("username", req.cookies.get("username"));
-		q.first({
-			success: function(result) {
-				res.json({
-					username: result.get("username"),
-					email: result.get("email"),
-					major: result.get("major"),
-					emailVerified: result.get("emailVerified")
-				});
-				console.log("sent /login.api result for user " + result.get("username"));
-			},
-			error: function(error) {
-				res.json({
-					error: "username not found or not logged in"
-				});
-				console.log("error processing /login.api request for user " 
-					+ req.cookies.get("username"));
-			}
-		});
+		var username = req.cookies.get("username");
+		if (username) {
+			var q = new Parse.Query(Parse.User).equalTo("username", username);
+			q.first({
+				success: function(result) {
+					res.json({
+						username: result.get("username"),
+						email: result.get("email"),
+						major: result.get("major"),
+						emailVerified: result.get("emailVerified")
+					});
+					console.log("sent /login.api result for user " + result.get("username"));
+				},
+				error: function(error) {
+					res.json({
+						error: "username not found or not logged in"
+					});
+					console.log("error processing /login.api request for user " 
+						+ req.cookies.get("username"));
+				}
+			});
+		} else {
+			// No cookie set for user. Punt to login page.
+			res.location("/login");
+			res.redirect("/login");
+		}
+
 	});
 
 	var crypto = require('crypto');
