555d2f596269c93c2411a11131452ca281c4d003
deathgaze <ksefchik.dev@gmail.com>
2014-05-22 11:45:29 -0500

    remove extraneous vars

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,8 +1,4 @@
 $(function(){
-    var validatedPassword = false;
-    var validatedEmail = false;
-    var validatedMajor = false;
-
     // found here: http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
     function testEmail(email) { 
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
