cda35164ebf489b6b778d35610c713457e168518
deathgaze <ksefchik.dev@gmail.com>
2014-05-22 00:16:33 -0500

    add cookie support

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 var parse = require('parse').Parse;
+var cookies = require('cookies').express;
+var keys = require('keygrip')([':-2>LE/he3DKsfBK$!q&P*;[SBohhhv)0N&lY*mg2#zL{QBfON.|JA0;E?%?j?Jr',
+					'{|FlPth>,jl5c&i*:IX|T|`PsY1-}em/)9<_geT!o~O*Ofggm+6I*fm|7oDDA;B[',
+					'iFnX43-Ax)(0:=3dZo8P@v/%{sglbq98S=5J&$gs:s))`+`H[h<D|<$f+`rG$>0n',
+					'luv_Zm*/B{rZ@?b;+<>b4j#s|{/nH$VIv?>BDKI,&3yJmDP~qo3?Xo$+K]/+!k(-']);
 parse.initialize("wJgTTR7HF7a0uUlc5IqyZVwL0IbCfk6WD9QRNdFf", "govCyUnlBTYzWW6PO6zhbAVOtwtdFS6r6HJ4e5u0");
 var app = express();
 
@@ -15,6 +20,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(cookies(keys));
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
